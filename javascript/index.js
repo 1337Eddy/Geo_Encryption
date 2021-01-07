982892dc947ebf0ec65df9a117bde41e1c0d42b2
Adrian Furrer <adrian.furrer1999@gmail.com>
2021-01-07 23:42:32 +0100

    styling and password message functionality

--- javascript/index.js
+++ javascript/index.js
@@ -29,18 +29,38 @@ function onSelectMapEnc() {
     }
 }
 
-function btn() {
-    if (document.querySelector("#pwEncryptionActivate").checked == true) {
-        document.querySelector("#pwEncryptionActivate").checked = false;
-    } else {
-        document.querySelector("#pwEncryptionActivate").checked = true;
+function encrypt() {
+    if (document.querySelector("#pwEnc").value !== document.querySelector("#pwEncRepeat").value) {
+        document.querySelector("#pwError").style.display = 'block';
+    } else if (document.querySelector("#pwEnc").value == "") {
+        document.querySelector("#pwNotUsed").style.display = 'block';
     }
 }
 
-function pwError() {
+function pwMsg() {
+    document.querySelector("#pwNotUsed").style.display = 'none';
     if (document.querySelector("#pwEnc").value !== document.querySelector("#pwEncRepeat").value) {
         document.querySelector("#pwError").style.display = 'block';
+        document.querySelector("#weak").style.display = 'none';
+        document.querySelector("#middle").style.display = 'none';
+        document.querySelector("#strong").style.display = 'none';
     } else {
-        document.querySelector("#pwError").style.display = 'none';
+        deactivatePwMsg();
+        if (document.querySelector("#pwEnc").value.length < 5 && document.querySelector("#pwEnc").value.length > 0) {
+            console.log(document.querySelector("#pwEnc").value.toString.length);
+            document.querySelector("#weak").style.display = 'block';
+        } else if (document.querySelector("#pwEnc").value.length < 8 && document.querySelector("#pwEnc").value.length > 0) {
+            document.querySelector("#middle").style.display = 'block';
+        } else if (document.querySelector("#pwEnc").value.length >= 8 && document.querySelector("#pwEnc").value.length > 0) {
+            document.querySelector("#strong").style.display = 'block';
+        }
     }
+}
+
+function deactivatePwMsg() {
+    document.querySelector("#weak").style.display = 'none';
+    document.querySelector("#middle").style.display = 'none';
+    document.querySelector("#strong").style.display = 'none';
+    document.querySelector("#pwNotUsed").style.display = 'none';
+    document.querySelector("#pwError").style.display = 'none';
 }
