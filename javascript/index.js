c7bf78891305b834e727c6d7d66103c4821182f6
Adrian Furrer <adrian.furrer1999@gmail.com>
2021-01-07 16:12:55 +0100

    new bootstrap structure

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,46 @@
-var pwEncCheckBox = document.querySelector('pwEncryptionActivate');
-pwEncCheckBox.onClick() = function() {
-    alert(pwEncCheckBox.checked)
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent(e.latlng.toString())
+        .openOn(mymap);
 }
 
-var btnEncrypt = document.querySelector('encrypt');
-btnEncrypt.onClick = function() {
-    alert("Clicked")
+
+function onMapClick1(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent(e.latlng.toString())
+        .openOn(mymap1);
 }
 
-document.querySelector('html').onclick = function() {
-    alert('Hey! Nicht so viel klicken!');
+function onSelectPwEnc() {
+    if (document.querySelector("#pwEncryptionActivate").checked == true) {
+        document.querySelector("#pwFields").style.display = 'block';
+    } else {
+        document.querySelector("#pwFields").style.display = 'none';
+    }
+}
+
+function onSelectMapEnc() {
+    if (document.querySelector("#encMapActivate").checked == true) {
+        document.querySelector("#encMap").style.display = 'block';
+    } else {
+        document.querySelector("#encMap").style.display = 'none';
+    }
+}
+
+function btn() {
+    if (document.querySelector("#pwEncryptionActivate").checked == true) {
+        document.querySelector("#pwEncryptionActivate").checked = false;
+    } else {
+        document.querySelector("#pwEncryptionActivate").checked = true;
+    }
+}
+
+function pwError() {
+    if (document.querySelector("#pwEnc").value !== document.querySelector("#pwEncRepeat").value) {
+        document.querySelector("#pwError").style.display = 'block';
+    } else {
+        document.querySelector("#pwError").style.display = 'none';
+    }
 }
